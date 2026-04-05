c16b0bef9e7f8c1f6fca7bd93c1f2854e862e1fa
Rizona <rizonehyseni@gmail.com>
2026-04-06 00:00:44 +0200

    Krijova klasen TrifidCipher me konstruktor dhe atribute

--- TrifidCipher.py
+++ TrifidCipher.py
@@ -1,2 +1,9 @@
 import string
 
+class TrifidCipher:
+    def __init__(self, key="", period=5, extra_char="+"):
+        self.period = period
+        self.extra_char = extra_char
+        self.alphabet = ""
+        self.letter_to_coord = {}
+        self.coord_to_letter = {}

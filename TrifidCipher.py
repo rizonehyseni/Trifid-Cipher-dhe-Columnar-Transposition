25d3eb85c73fc73e119fd12bdbbc67d5b4c85ec1
Rizona <rizonehyseni@gmail.com>
2026-04-11 21:53:42 +0200

    Funksioni per gjenerimin e alfabetit ne trific cipher

--- TrifidCipher.py
+++ TrifidCipher.py
@@ -7,3 +7,27 @@ class TrifidCipher:
         self.alphabet = ""
         self.letter_to_coord = {}
         self.coord_to_letter = {}
+
+    def _generate_mixed_alphabet(self, key, extra):
+        key = key.upper().replace(" ", "")
+        seen = set()
+        alphabet = []
+        
+        for char in key:
+            if char.isalpha() and char not in seen:
+                alphabet.append(char)
+                seen.add(char)
+        
+        for char in string.ascii_uppercase:
+            if char not in seen:
+                alphabet.append(char)
+                seen.add(char)
+        
+        if extra not in seen:
+            alphabet.append(extra)
+        
+        alphabet = alphabet[:27]
+        while len(alphabet) < 27:
+            alphabet.append(chr(65 + len(alphabet)))  
+        
+        return ''.join(alphabet)    
